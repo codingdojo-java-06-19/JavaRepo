c2d2dd764992ee3dfeb6c053493e07ea294b0491
AlexanderCaves <ahcaves@gmail.com>
2019-06-09 13:27:33 -0700

    zookeeper1

---
+++ ZooKeeperI/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+	public static void main(String[] args) {
+		Gorilla Koko = new Gorilla();
+		
+		Koko.displayEnergy();
+		Koko.throwSomething();
+		Koko.throwSomething();
+		Koko.throwSomething();
+		Koko.eatBanana();
+		Koko.eatBanana();
+		Koko.climb();
+		Koko.displayEnergy();
+	}
+}
